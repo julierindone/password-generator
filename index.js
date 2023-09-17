d921a000052ca9046f5ecec4b8ebf3144ab857e6
Julie Rindone <julie.rindone@gmail.com>
2023-09-16 22:20:00 -0500

    Completed most js & css

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-"~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+  "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
+let passwordBoxes = document.getElementById("boxes")
+let passwordOne = "cats12345678" // characters chosen at random
+let passwordOneEl = document.getElementById("pw-one")
+let passwordTwo = "dogs12345678"
+let passwordTwoEl = document.getElementById("pw-two")
 
+function generatePasswords() {
+  // display the boxes:
+  passwordBoxes.style.display = "flex"
 
+  // display passwords
+  passwordOneEl.innerText = passwordOne
+  passwordTwoEl.innerText = passwordTwo
+}
+
+function makePassword() {
+  const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+  let pw = ""
+  
+  for(let i = 0; i < 16; i++) {
+    let index= Math.floor(Math.random() * characters.length)
+    pw += characters[index]
+    console.log(pw)
+  }
+
+  // console.log(characters[index])
+}
+randomPassword()
 
