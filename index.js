bb2f462ef36bba321fd96cebd54bd4154396fe94
Julie Rindone <julie.rindone@gmail.com>
2023-09-17 21:58:37 -0500

    Completed makePassword function, made layout more responsive, cleaned up code.

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
   "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
   "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
+const lettersAndNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 let passwordBoxes = document.getElementById("boxes")
-let passwordOne = "cats12345678" // characters chosen at random
+let passwordOne = ""
 let passwordOneEl = document.getElementById("pw-one")
-let passwordTwo = "dogs12345678"
+let passwordTwo = ""
 let passwordTwoEl = document.getElementById("pw-two")
 
 function generatePasswords() {
   // display the boxes:
   passwordBoxes.style.display = "flex"
+
+  // make passwords
+  passwordOne = makePassword()
+  passwordTwo = makePassword()
 
   // display passwords
   passwordOneEl.innerText = passwordOne
@@ -19,16 +27,11 @@ function generatePasswords() {
 }
 
 function makePassword() {
-  const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
   let pw = ""
-  
-  for(let i = 0; i < 16; i++) {
-    let index= Math.floor(Math.random() * characters.length)
+
+  for (let i = 0; i < 16; i++) {
+    let index = Math.floor(Math.random() * characters.length)
     pw += characters[index]
-    console.log(pw)
   }
-
-  // console.log(characters[index])
+  return pw
 }
-randomPassword()
-
